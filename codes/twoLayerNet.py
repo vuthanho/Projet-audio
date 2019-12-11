ddf21e288523c0434ce5424959399f040e465693
Loïc <loic.argentier@gmail.com>
2019-12-11 23:33:31 +0100

    Test reconstruction

--- codes/twoLayerNet.py
+++ codes/twoLayerNet.py
@@ -6,7 +6,6 @@ Created on Tue Nov 26 22:08:20 2019
 """
 
 import torch
-import matplotlib.pyplot as plt
 
 # Fully Convolutionnal Network
 
@@ -47,32 +46,6 @@ class FCN(torch.nn.Module):
         x = self.firstlayer(x)
         x = self.hiddenlayer(x)
         x = self.lastlayer(x)
-        return x
-
-
-class TwoLayerNet(torch.nn.Module):
-    #Constructeur qui définis nos couches
-    def __init__(self, D_in, H, D_out):
-        """
-        - N = batch size; D_in = input dimension;
-        - H = hidden dimension; D_out = output dimension.
-        """
-        super(TwoLayerNet, self).__init__()
-        self.first_conv = torch.nn.Conv2d(D_in, H, 5)
-        self.second_conv = torch.nn.Conv2d(H, D_out, 5)
-        # self.first_fully_c = torch.nn.Linear(H,1)
-        
-    #forward : calcul a partir de l'entrée la sortie du réseau en appliquant les différentes couches successif définis dans le constructeur
-    def forward(self, x):
-#        x=x.to(torch.device("cuda:0"))
-        x = torch.nn.functional.relu(self.first_conv(x))
-        for i in range(0,10):
-            plt.pcolormesh(x[i][0].cpu().detach().numpy())
-            plt.show()
-        x = torch.nn.functional.relu(self.second_conv(x))
-        # x = x.view(-1, 24363600)
-        # x = torch.nn.functional.relu(self.first_fully_c(x))
-
         return x
 
 
