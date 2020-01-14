c8d2b1c2e2e2aa32c23222f97b0b602b2e4dae9f
Loïc <loic.argentier@gmail.com>
2020-01-14 08:48:34 +0100

    add script reprise train

--- reprise_train.py
+++ reprise_train.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 08:16:52 2020
+
+@author: Loïc
+"""
+
+# -*- coding: utf-8 -*-
 
 """
 Created on Tue Nov 26 16:22:59 2019
@@ -20,7 +27,16 @@ import sys
 import math
 from scipy.io import wavfile #for audio processing
 import scipy.signal as sig
-from time import sleep
+
+def signal_reconsctructed(module_s,phase_s,indice):
+     fs=8000
+     nperseg = 256
+     noverlap=nperseg//2
+     Zxx = module_s*(np.exp(1j*phase_s))
+     _,reconstructed = sig.istft(Zxx, fs=fs, window='hann', nperseg=nperseg, noverlap=noverlap, nfft=None, input_onesided=True, boundary=True, time_axis=-1, freq_axis=-2)
+     reconstructed = np.int16(reconstructed/np.amax(np.absolute(reconstructed))*2**15)
+#     wavfile.write('signal_denoise_'+str(indice)+'.wav',fs,reconstructed)
+     return reconstructed
 
 def psnr(img1,img2):
     img1 = img1.astype(np.float64)
@@ -29,73 +45,51 @@ def psnr(img1,img2):
     result = 10 * math.log10(1. / mse)
     return result
 
-#Batch size (permet de travailler avec plusieurs sample en même temps )
-"""
-attention ! il faut vérifier que sa donne un résultat entier nb de fichier 
-divisé par batch_size (enfin je pense)
-"""
 
-batch_size= 5
+
+
+
+#Variable a set
+
+batch_size=5
+n_iterations =100
+display_spectro=False
+display_psnr=True
+
 
 #get the workspace path
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cwd = os.getcwd()
-#DL training set
+
+#Affichage de ce qu'il contient
+#for param_tensor in model_load.state_dict():
+#    print(param_tensor, "\t", model_load.state_dict()[param_tensor].size())
+
+#load model
+model_load = FCN()
+model_load.load_state_dict(torch.load(cwd+"\\saved\\model_b5_7000_ASVG"))
+model_load.double().cuda()
+model_load.eval()
+
+
+torch.backends.cudnn.enabled = True
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer=torch.optim.ASGD(model_load.parameters())
+optimizer.load_state_dict(torch.load(cwd+"\\saved\\optimizer_b5_7000_ASVG"))
+
+
+# A PARTIR DE LA FAUT METTRE EN ORDRE
+
 train_bruit_path = cwd+'/data/data_train_bruit'
 train_path = cwd+'/data/data_train'
+
 trainset = SpeechDataset(train_bruit_path, train_path, transform=['reshape','cut&sousech','normalisation','train','tensor_cuda'])
-
-#training set loader
-"""
-Le data loader est une fonction qui permet d'importer les données de manière itératifs, 
-le but étant de procéder aux calculs sur une nombre d'échantillons restreint (égale au
-batch_size).
-- shuffle : importer les échantillons de manière aléatoire
-- num_workers : nombre de coeur du processeur utilisés 
-"""
 trainloader=torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
-
-#DL test set
-test_bruit_path = cwd+'/data/data_test_bruit'
-test_path = cwd+'/data/data_test'
-testset =  SpeechDataset(test_bruit_path, test_path, transform=['reshape','cut&sousech','normalisation','test','tensor_cuda'])
-
-#training set loader
-testloader=torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=0)
 
 #data set as iterator
 dataiter=iter(trainloader)
 
-#nombre de batches totales présent dans notre sytèmes
-n_batches=len(dataiter)
-
-# Construct our model by instantiating the class defined above
-model = FCN()
-def init_normal(m):
-        if type(m) == torch.nn.Linear:
-            torch.nn.init.dirac_(m.weight, std=0.01)
-model.apply(init_normal)
-model.double().cuda()
-
-#learning rate
-#learning_rate = 1e-6
-learning_rate = 1e-6
-#nb d'iter → nombre epoch
-n_iterations =7000
-
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
-criterion = torch.nn.MSELoss(reduction='sum')
-torch.backends.cudnn.enabled = True
-#decente par gradient, avoir si on prend autre chose
-# optimizer = torch.optim.SGD(model.parameters(), lr= learning_rate)
-optimizer = torch.optim.ASGD(model.parameters(),lr=learning_rate)
 loss_vector = np.zeros(n_iterations)
-
-# for epoch in range(n_iterations):
-#     print(epoch)
-
 psnr_debruite_vector = np.zeros(n_iterations)
 psnr_bruite_vector = np.zeros(n_iterations)
 
@@ -121,7 +115,7 @@ for epoch in range(n_iterations):
 
         x_temp=x[:,:, :,subpart:subpart+8]
         y_temp=y[:,:, :,subpart:subpart+1]
-        y_pred_temp = model(x_temp)
+        y_pred_temp = model_load(x_temp)
         
         # Compute and print loss
         loss = criterion(torch.squeeze(y_pred_temp), torch.squeeze(y_temp))
@@ -187,39 +181,5 @@ for epoch in range(n_iterations):
     dataiter=iter(trainloader)
     
 #save model & optimizer : https://pytorch.org/tutorials/beginner/saving_loading_models.html
-torch.save(model.state_dict(), cwd+"\\saved\\model_b5_7000_ASVG")
-torch.save(optimizer.state_dict(), cwd+"\\saved\\optimizer_b5_7000")
-
-def signal_reconsctructed(module_s,phase_s,indice):
-     fs=8000
-     nperseg = 256
-     noverlap=nperseg//2
-     Zxx = module_s*(np.exp(1j*phase_s))
-     _,reconstructed = sig.istft(Zxx, fs=fs, window='hann', nperseg=nperseg, noverlap=noverlap, nfft=None, input_onesided=True, boundary=True, time_axis=-1, freq_axis=-2)
-     reconstructed = np.int16(reconstructed/np.amax(np.absolute(reconstructed))*2**15)
-     wavfile.write('signal_denoise_'+str(indice)+'.wav',fs,reconstructed)
-     return reconstructed
-
-# signal_reconsctructed(y_pred,a,0)
-    
-def RSB(prediction,bruit,reference):
-    reference=reference[:,0:116]
-#    s_min = prediction.min()
-#    s_max = prediction.max()
-#    prediction = np.divide(prediction-s_min,s_max-s_min)
-    
-    bruit=bruit[:,0:116]
-    s_minb = bruit.min()
-    s_maxb = bruit.max()
-    bruit = np.divide(bruit-s_minb,s_maxb-s_minb)
-    
-    bruit=np.abs(bruit-reference)#puissance du bruit
-    bruit=np.sum(bruit,0)/129#moyenne de la puissance du bruit à chaque instant
-    
-    prediction=np.sum(np.abs(prediction),0)/129
-    
-    rsb=(np.sum(prediction)/116)/(np.sum(bruit)/116)#moyenne de la puissance du 
-    rsb = 10 * math.log10(rsb)
-    
-    return rsb
-
+#torch.save(model_load.state_dict(), cwd+"\\saved\\model_b5_7000_ASVG")
+#torch.save(optimizer.state_dict(), cwd+"\\saved\\optimizer_b5_7000")
